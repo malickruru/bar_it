ec8eea0e1ee42e11b422a6e63ed5f0e08ed3cae6
malickruru <rubs99rstd@gmail.com>
2023-01-19 19:01:50 +0000

    pre deployment

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
   <React.StrictMode>
     <App />
   </React.StrictMode>
